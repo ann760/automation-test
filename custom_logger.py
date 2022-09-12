9e6327cb5c1adf48c2120b14add39e6818c1ef75
Ann-Marie <anniedillier@gmail.com>
2022-09-12 14:45:52 -0700

    Update custom_logger.py

--- custom_logger.py
+++ custom_logger.py
@@ -3,15 +3,23 @@ import os
 import sys 
 from datetime import datetime
 
+filename = (os.path.basename(sys.argv[0]).split('.')[0])
+print(filename)
+
 class log_gen:
   @staticmethod
   def logen(__name__):
-    loggin.basicConfig(filename=os.path.join(f'logs/{datetime.now():%Y-%m-%d %H-%M-%S}-{sys.arvg{0}}.log'), format='%(asctime)s - %(levelname)s - %(name)s - %(message)s')
-    logging.basicConfig(logging.INFO)
-    # file_handler = loggin.FileHandler(f'logs/{datetime.now():%Y-%m-%d %H-%M-%S}-{sys.arvg{0}}.log')
-    # formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
-    # file_handler.setFormatter(formatter)
-    # logger.addHandler(filrhandler)    
-    return logger
+    logger = logging.getLogger(__name))
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
     
-  
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setFromantter(formantter)
+    
+    file_handler = loggin.FileHandler(os.path.join(f'logs/{datetime.now():%Y-%m-%d %H-%M-%S}-{filename}.log'), 'w', 'utf-8')    
+    file_handler.setFormatter(formatter)
+    
+    logger.addHandler(file_handler)  
+    logger.addhandler(stdout_handler)
+    
+    return logger
