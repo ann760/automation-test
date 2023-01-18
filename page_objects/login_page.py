1c09fdfb0cf84a9b79ceb4904d4e9dde65ccf527
Ann-Marie <anniedillier@gmail.com>
2023-01-18 10:32:12 -0800

    create POM

---
+++ page_objects/login_page.py
@@ -0,0 +1,5 @@
+# imports 
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
