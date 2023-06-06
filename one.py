00cf5d0d8dd61fa201e04483728ed3ba0b448171
Ann-Marie <anniedillier@gmail.com>
2023-06-05 22:26:43 -0700

    use google

--- one.py
+++ one.py
@@ -3,7 +3,8 @@
 # 2 check pip package is installed by typing pip into the cmd  
 # 3 install selenium by typing pip3 install selenium into the cmd
 # 4 install webdriver-manager by typing pip3 install webdriver-manager into the cmd
-# 5 get webdriver and place into the path 
+# 5 install msedge-selenium-tools by typing pip3 install msedge-selenium-tools selenium==3.141
+# 6 get webdriver and place into the path 
 
 #Selenium imports
 from selenium import webdriver
@@ -11,18 +12,29 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+from selenium.webdriver.edge.service import Service
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC 
 from selenium.webdriver.support.wait import WebDriverWait
 
 #Other imports
-import os
 import getpass
+import os
+import time
 
 
-PATH = r"C:\Users\annie\Documents\code\drivers\chromedriver.exe"
+PATH = r'C:/Users/annie/Documents/code/drivers/chromedriver.exe'
 # driver = webdriver.Edge(PATH)
+
+# driver = webdriver.Edge(r'C:/Users/annie/Documents/code/drivers/msedgedriver.exe')
+
+
+# options = Options()
+# options.binary_location = r"C:/Users/annie/Documents/code/drivers/msedgedriver.exe"
+# driver = webdriver.Edge(options = options)
+
 
 options = webdriver.ChromeOptions()
 options.add_argument("start-maximized")
@@ -31,18 +43,14 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), opti
 
 
 #Log in to insta
-my_password = getpass.getpass("What is your password?")
-driver.get("https://www.instagram.com/")
+# my_password = getpass.getpass("What is your password?")
+driver.get("https://www.google.com/")
 print(driver.title)
-username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name= 'username']")))
-password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name= 'password']")))
 
-username.clear()
-password.clear()
-username.send_keys("anniedillier@gmail.com")
-password.send_keys(my_password)
+search_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'q')))
+search_field.clear()
+search_field.send_keys("cats")
 
-login = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type= 'submit']"))).click()
-not_now = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Not Now')]"))).click()
-not_now = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Not Now')]"))).click()
+login = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'btnk'))).click()
+time.sleep(5)
 # driver.quit()
